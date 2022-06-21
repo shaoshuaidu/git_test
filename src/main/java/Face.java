f70ac59279acfb2598e5e40d93789e3ee6b6672b
dushaoshuai <shaoshuaidu@gmail.com>
2022-06-21 14:50:35 +0800

    third commit

--- src/main/java/Face.java
+++ src/main/java/Face.java
@@ -2,6 +2,7 @@ public class Face {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("hello git3");
 
     }
 }
