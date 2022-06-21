98f28e09edd43a173d1eed10ef694f3468f07775
dushaoshuai <shaoshuaidu@gmail.com>
2022-06-21 14:58:39 +0800

    hot-fix commit

--- src/main/java/Face.java
+++ src/main/java/Face.java
@@ -3,6 +3,7 @@ public class Face {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git4 test");
 
     }
 }
