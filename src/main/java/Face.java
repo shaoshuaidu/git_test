fc5184b6a594de6e4cd15a130f9bfcd3503468c0
dushaoshuai <shaoshuaidu@gmail.com>
2022-06-21 15:12:34 +0800

    push test commit

--- src/main/java/Face.java
+++ src/main/java/Face.java
@@ -6,6 +6,7 @@ public class Face {
         System.out.println("hello git4 test");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
 
