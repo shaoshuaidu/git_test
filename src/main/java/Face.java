a245d8cbb908b29094d24a2c51bdfc62a194a47d
dushaoshuai <shaoshuaidu@gmail.com>
2022-06-21 14:48:00 +0800

    second commit

--- src/main/java/Face.java
+++ src/main/java/Face.java
@@ -1,5 +1,8 @@
 public class Face {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+
     }
 }
+
